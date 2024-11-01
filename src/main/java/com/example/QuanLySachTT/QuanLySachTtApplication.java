ddf8c97add54c1820d35ef71cf22aee5c0bbd409
TrucDuy <TrucDuy@LAPTOP-3K2TB8FV>
2024-11-01 21:43:09 +0700

    tao moi du an

---
+++ src/main/java/com/example/QuanLySachTT/QuanLySachTtApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuanLySachTT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLySachTtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLySachTtApplication.class, args);
+	}
+
+}
